c9723dfe292dd3fe4aa682239a16b47f048b277c
krishna dupatane <72943224+dupatane@users.noreply.github.com>
2023-05-31 00:49:40 +0530

    Create git(1)puhook.sh

---
+++ git(1)puhook.sh
@@ -0,0 +1,5 @@
+# How do you set up a script to run every time a repository receives new commits through push?
+#!/bin/bash
+
+echo "You can use this hook to perform actions such as updating a deployment, running tests, 
+or triggering a build process based on the received commits"
