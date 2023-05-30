53216148e4381d311a7f28de19800f26573bbe16
krishna dupatane <72943224+dupatane@users.noreply.github.com>
2023-05-30 20:57:00 +0530

    Update PRINT(1-100).sh

--- PRINT(1-100).sh
+++ PRINT(1-100).sh
@@ -1,9 +1,9 @@
-#Write a short program that prints each number from 1 to 100 on a new line#
-
 #!/bin/bash
 i=1
 while [ $i -le 100 ]; do
-if [ `expr $i % 3` -eq 0 ]; then
+if [ `expr $i % 3` -eq 0 ] && [ `expr $i % 5` -eq 0 ]; then
+echo "AVAAMO"
+elif [ `expr $i % 3` -eq 0 ]; then
 echo "AVA"
 elif [ `expr $i % 5` -eq 0 ]; then
 echo "AMO"
@@ -12,4 +12,3 @@ echo "$i "
 fi
 i=`expr $i + 1`
 done
-~
