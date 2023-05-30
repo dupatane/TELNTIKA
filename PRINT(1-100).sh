d41578e1d0737c085c4bab31b89fea12c48ba334
krishna dupatane <72943224+dupatane@users.noreply.github.com>
2023-05-30 20:48:03 +0530

    Update PRINT(1-100).sh

--- PRINT(1-100).sh
+++ PRINT(1-100).sh
@@ -3,6 +3,13 @@
 #!/bin/bash
 i=1
 while [ $i -le 100 ]; do
+if [ `expr $i % 3` -eq 0 ]; then
+echo "AVA"
+elif [ `expr $i % 5` -eq 0 ]; then
+echo "AMO"
+else
 echo "$i "
+fi
 i=`expr $i + 1`
 done
+~
